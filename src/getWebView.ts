deb023ec66ccc42f09422fe005d4ed87ea741648
Sreejith R <srramakr@microsoft.com>
2019-07-22 12:46:13 +0530

    Initial commit

---
+++ src/getWebView.ts
@@ -0,0 +1,8 @@
+const webViewContent = `
+<!DOCTYPE html>
+  <body>
+    <iframe style="height: 100vh; width: 100vw" src="http://dev.local"></iframe>
+  </body>
+</html>
+`;
+export default webViewContent;
